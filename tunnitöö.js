8cf5116d60e3c07b78cbb1d9751e8039cc76fe0a
EVassar <90027736+EVassar@users.noreply.github.com>
2021-09-03 16:05:34 +0300

    Muutsin nii, et enam ei viska kui pole piisavalt raha siis ei viska ette seda, et sa selle ostsid.

--- tunnitöö.js
+++ tunnitöö.js
@@ -1,16 +1,16 @@
-confirmAction==false
 Rahakott = Math.random()*50  //$ randomiseerib, kui palju raha kliendil onn
 tooteHind = 25               // kui palju toode maksab
 rahaarv = (Rahakott-tooteHind)
 if (Rahakott<tooteHind){
-    alert("Sa ei saa seda osta, sul jäi puudu" +rahaarv)
-}  
+    alert("Sa ei saa seda osta, sul jäi puudu " +rahaarv)
+}
 if (Rahakott>tooteHind){ 
     confirmAction = confirm ("Kas sa soovid seda osta");
-}
-if (confirmAction==false){
-    alert ("Sa ei ostnud seda, sul on" +Rahakott +" eurot üle");     
-}
-else {
-    alert ("Sa ostsid selle sul on üle" +rahaarv +" eurot järel"); 
+
+    if (confirmAction==false){
+        alert ("Sa ei ostnud seda, sul on " +Rahakott +" eurot üle");     
+    }
+     else 
+         alert ("Sa ostsid selle sul on üle " +rahaarv +" eurot järel"); 
+    
 }
