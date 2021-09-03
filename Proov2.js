e8ebf42b1ac419d412d079d427b789f34342b12a
EVassar <90027736+EVassar@users.noreply.github.com>
2021-09-03 13:20:02 +0300

    Natukene katkine, aga töötab.

--- Proov2.js
+++ Proov2.js
@@ -1,4 +1,3 @@
-confirmAction==false
 Rahakott = Math.random()*50  //$ randomiseerib, kui palju raha kliendil onn
 tooteHind = 25               // kui palju toode maksab
 rahaarv = (Rahakott-tooteHind)
@@ -9,8 +8,7 @@ if (Rahakott>tooteHind){
     confirmAction = confirm ("Kas sa soovid seda osta");
 }
 if (confirmAction==false){
-    alert ("Sa ei ostnud seda, sul on" +Rahakott +" eurot üle");     
-}
-else {
-    alert ("Sa ostsid selle sul on üle" +rahaarv +" eurot järel"); 
-}
+     alert ("Sa ei ostnud seda, sul on" +Rahakott +"eurot üle");     
+else{
+     alert ("Sa ostsid selle sul on üle" +rahaarv +"eurot järel"); 
+}   
