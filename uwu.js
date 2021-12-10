0cc59a0198893273c7a14e4cf63c55f43c51e16c
EVassar <90027736+EVassar@users.noreply.github.com>
2021-12-10 12:45:55 +0200

    Add files via upload

---
+++ uwu.js
@@ -0,0 +1,11 @@
+for (let i = 0; i <= 100; i+=1){
+    if (i % 5 == 0, i % 3 == 0){
+        console.log ('foobar ' + i);
+    }
+    else if (i % 3 == 0){
+        console.log ('foo ' + i);
+    }
+    else if (i % 5 == 0){
+        console.log ('bar ' + i);
+    }
+}
